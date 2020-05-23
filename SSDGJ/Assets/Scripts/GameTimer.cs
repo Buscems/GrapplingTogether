32e17e6f471265264af073f81e6b1438cd6fce5b
NolanSadowski <47158785+NolanSadowski@users.noreply.github.com>
2020-05-23 09:25:19 -0400

    level 1 yells at you for saying bad words

---
+++ SSDGJ/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameTimer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public static float time = 300;
+
+    void Start()
+    {
+        StartCoundownTimer();
+    }
+
+    void StartCoundownTimer()
+    {
+        if (timerText != null)
+        {
+            time = 300;
+            timerText.text = "Time Left: 5:00";
+            InvokeRepeating("UpdateTimer", 0.0f, 0.01667f);
+        }
+    }
+
+    void UpdateTimer()
+    {
+        if (timerText != null)
+        {
+            time -= Time.deltaTime;
+            string minutes = Mathf.Floor(time / 60).ToString("0");
+            string seconds = (time % 60).ToString("00");
+            string fraction = ((time * 100) % 100).ToString("000");
+            timerText.text = minutes + ":" + seconds;
+        }
+    }
+}
--- SSDGJ/Assets/Scripts/VoiceRecognition.cs
+++ SSDGJ/Assets/Scripts/VoiceRecognition.cs
@@ -4,13 +4,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
 using System.Linq;
+using UnityEngine.UI;
 
 public class VoiceRecognition : MonoBehaviour
 {
     private KeywordRecognizer keyWordRecognizer;
     private Dictionary<string, Action> actions = new Dictionary<string, Action>();
 
-    public float difficultyScale = 1f;
+    public int curseCounter;
+    public Image red;
+    public float x;
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +35,9 @@ public class VoiceRecognition : MonoBehaviour
         actions.Add("cock", MakeHarder);
         actions.Add("cocksucker", MakeHarder);
 
-        keyWordRecognizer = new KeywordRecognizer(actions.Keys.ToArray(), ConfidenceLevel.Medium);
+        actions.Add("sorry", Apologize);
+
+        keyWordRecognizer = new KeywordRecognizer(actions.Keys.ToArray(), ConfidenceLevel.Low);
         keyWordRecognizer.OnPhraseRecognized += RecognizedSpeech;
         keyWordRecognizer.Start();
     }
@@ -40,7 +45,11 @@ public class VoiceRecognition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        x = .15f * curseCounter;
+        if(x >= .75f)
+        {
+            x = .75f;
+        }
     }
 
     private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
@@ -51,8 +60,16 @@ public class VoiceRecognition : MonoBehaviour
 
     private void MakeHarder()
     {
-        //increase difficulty here
-        difficultyScale += 1;
+        curseCounter += 1;
+        ShakeBehavior.shakeDuration = .5f;
+        GameTimer.time -= 10f;
+        red.color = new Color(1, 0, 0, x);
+    }
+
+    private void Apologize()
+    {
+        curseCounter = 0;
+        red.color = new Color(1, 0, 0, 0);
     }
 }
 
