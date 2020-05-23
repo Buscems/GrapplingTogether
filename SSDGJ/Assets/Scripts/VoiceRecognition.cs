7ff2bdc90e72cf874848d87c10cb712957d737e7
NolanSadowski <47158785+NolanSadowski@users.noreply.github.com>
2020-05-23 12:26:20 -0400

    added music to lv1

--- SSDGJ/Assets/Scripts/VoiceRecognition.cs
+++ SSDGJ/Assets/Scripts/VoiceRecognition.cs
@@ -12,28 +12,19 @@ public class VoiceRecognition : MonoBehaviour
     private Dictionary<string, Action> actions = new Dictionary<string, Action>();
 
     public int curseCounter;
-    public Image red;
-    public float x;
 
     // Start is called before the first frame update
     void Start()
     {
         actions.Add("fuck", MakeHarder);
-        actions.Add("shit", MakeHarder);
-        actions.Add("shitting", MakeHarder);
-        actions.Add("bitch", MakeHarder);
-        actions.Add("ass", MakeHarder);
-        actions.Add("cunt", MakeHarder);
-        actions.Add("motherfucker", MakeHarder);
-        actions.Add("slut", MakeHarder);
-        actions.Add("hoe", MakeHarder);
-        actions.Add("whore", MakeHarder);
         actions.Add("fucking", MakeHarder);
-        actions.Add("twat", MakeHarder);
-        actions.Add("pussy", MakeHarder);
-        actions.Add("dick", MakeHarder);
-        actions.Add("cock", MakeHarder);
-        actions.Add("cocksucker", MakeHarder);
+        actions.Add("motherfucker", MakeHarder);
+        actions.Add("what the fuck", MakeHarder);
+        actions.Add("fuck this", MakeHarder);
+        actions.Add("fuck off", MakeHarder);
+        actions.Add("are you fucking kidding me", MakeHarder);
+        actions.Add("come the fuck on", MakeHarder);
+        actions.Add("shit", MakeHarder);
 
         actions.Add("sorry", Apologize);
 
@@ -45,11 +36,7 @@ public class VoiceRecognition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        x = .15f * curseCounter;
-        if(x >= .75f)
-        {
-            x = .75f;
-        }
+
     }
 
     private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
@@ -63,13 +50,15 @@ public class VoiceRecognition : MonoBehaviour
         curseCounter += 1;
         ShakeBehavior.shakeDuration = .5f;
         GameTimer.time -= 10f;
-        red.color = new Color(1, 0, 0, x);
     }
 
     private void Apologize()
     {
-        curseCounter = 0;
-        red.color = new Color(1, 0, 0, 0);
+        if (curseCounter >= 1)
+        {
+            curseCounter = 0;
+            GameTimer.time += 10f;
+        }
     }
 }
 
